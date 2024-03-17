bfca955af96861db6734402858faf5fb60ed14a2
Marcin Wardzinski <wardzinskim@outlook.com>
2024-03-17 08:59:20 +0100

    db migration added

--- backend/Core/MyBudget.Infrastructure/Migrations/20240317074122_CreateTransferCategoriesTable.Designer.cs
+++ backend/Core/MyBudget.Infrastructure/Migrations/20240317074122_CreateTransferCategoriesTable.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MyBudget.Infrastructure.Database;
 
@@ -11,9 +12,11 @@ using MyBudget.Infrastructure.Database;
 namespace MyBudget.Infrastructure.Migrations
 {
     [DbContext(typeof(BudgetContext))]
-    partial class BudgetContextModelSnapshot : ModelSnapshot
+    [Migration("20240317074122_CreateTransferCategoriesTable")]
+    partial class CreateTransferCategoriesTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
