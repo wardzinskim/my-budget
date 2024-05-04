37b6f629e11c80f5d64753a675e51426502193aa
Marcin Wardzinski <wardzinskim@outlook.com>
2024-05-04 22:37:35 +0200

    add get budget totals query

---
+++ backend/Core/MyBudget.Application/Budgets/GetBudgetTotals/GetBudgetTotalsQuery.cs
@@ -0,0 +1,10 @@
+﻿using MassTransit.Mediator;
+using MyBudget.SharedKernel;
+
+namespace MyBudget.Application.Budgets.GetBudgetTotals;
+
+
+public record GetBudgetTotalsQuery(Guid BudgetId, int? Year = null, int? Month = null) 
+    : Request<Result<BudgetTotals>>;
+
+public record BudgetTotals(decimal Incomes, decimal Expenses);
---
+++ backend/MyBudget.Api/Features/Core/BudgetStatisticsModule.cs
@@ -0,0 +1,39 @@
+﻿using Carter;
+using Carter.OpenApi;
+using MassTransit;
+using MassTransit.Mediator;
+using Microsoft.AspNetCore.Mvc;
+using MyBudget.Api.Extensions;
+using MyBudget.Application.Budgets.GetBudgetTotals;
+
+namespace MyBudget.Api.Features.Core;
+
+public class BudgetStatisticsModule : ICarterModule
+{
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/budget/{id:guid}/totals", GetBudgetTotals)
+            .WithName(nameof(GetBudgetTotals))
+            .WithTags("budget-statistics")
+            .Produces(StatusCodes.Status200OK, typeof(BudgetTotals))
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .ProducesProblem(StatusCodes.Status403Forbidden)
+            .WithOpenApi()
+            .IncludeInOpenApi();
+    }
+
+
+    private static async Task<IResult> GetBudgetTotals(
+        IMediator mediator,
+        [FromRoute] Guid id,
+        [FromQuery] int? year,
+        [FromQuery] int? month,
+        CancellationToken cancellationToken
+    )
+    {
+        var result = await mediator.SendRequest(
+            new GetBudgetTotalsQuery(id, year, month), cancellationToken);
+
+        return result.Match((x) => Results.Ok(x));
+    }
+}
