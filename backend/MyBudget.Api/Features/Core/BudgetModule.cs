df6204157effd32b2b3ea2651bdcf8bb15aa67b2
Marcin Wardzinski <wardzinskim@outlook.com>
2024-05-06 09:28:52 +0200

    extract category module

--- backend/MyBudget.Api/Features/Core/BudgetModule.cs
+++ backend/MyBudget.Api/Features/Core/BudgetModule.cs
@@ -1,68 +1,45 @@
 ﻿using Carter;
-using Carter.OpenApi;
 using MassTransit;
 using MassTransit.Mediator;
 using Microsoft.AspNetCore.Mvc;
 using MyBudget.Api.Extensions;
-using MyBudget.Application.Budgets.ArchiveBudgetCategory;
 using MyBudget.Application.Budgets.CreateBudget;
-using MyBudget.Application.Budgets.CreateBudgetCategory;
 using MyBudget.Application.Budgets.GetBudget;
 using MyBudget.Application.Budgets.GetBudgets;
 using MyBudget.Application.Budgets.Model;
 
 namespace MyBudget.Api.Features.Core;
 
-public class BudgetModule : ICarterModule
+public class BudgetModule : CarterModule
 {
-    public void AddRoutes(IEndpointRouteBuilder app)
+    public BudgetModule() : base("budget")
     {
-        app.MapPost("/budget", CreateBudget)
-            .WithName(nameof(CreateBudget))
-            .WithTags("budget")
+        WithTags("budget");
+        IncludeInOpenApi();
+    }
+
+    public override void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapPost("", CreateBudget).WithName(nameof(CreateBudget))
             .Produces(StatusCodes.Status201Created)
             .ProducesValidationProblem()
-            .WithOpenApi()
-            .IncludeInOpenApi();
+            .WithOpenApi();
 
-        app.MapGet("/budget", GetBudgets)
+        app.MapGet("", GetBudgets)
             .WithName(nameof(GetBudgets))
             .WithTags("budget")
             .Produces<IEnumerable<BudgetDTO>>(StatusCodes.Status200OK)
             .ProducesProblem(StatusCodes.Status400BadRequest)
-            .WithOpenApi()
-            .IncludeInOpenApi();
+            .WithOpenApi();
 
-        app.MapGet("/budget/{id:guid}", GetBudget)
+        app.MapGet("{id:guid}", GetBudget)
             .WithName(nameof(GetBudget))
             .WithTags("budget")
             .Produces<BudgetDTO>(StatusCodes.Status200OK)
             .ProducesProblem(StatusCodes.Status400BadRequest)
             .ProducesProblem(StatusCodes.Status404NotFound)
             .ProducesProblem(StatusCodes.Status403Forbidden)
-            .WithOpenApi()
-            .IncludeInOpenApi();
-
-
-        app.MapPost("/budget/{id:guid}/category", CreateBudgetCategory)
-            .WithName(nameof(CreateBudgetCategory))
-            .WithTags("budget")
-            .Produces(StatusCodes.Status201Created)
-            .ProducesProblem(StatusCodes.Status404NotFound)
-            .ProducesProblem(StatusCodes.Status403Forbidden)
-            .ProducesValidationProblem()
-            .WithOpenApi()
-            .IncludeInOpenApi();
-
-        app.MapPut("/budget/{id:guid}/category/{name}/archive", ArchiveBudgetCategory)
-            .WithName(nameof(ArchiveBudgetCategory))
-            .WithTags("budget")
-            .Produces(StatusCodes.Status204NoContent)
-            .ProducesProblem(StatusCodes.Status404NotFound)
-            .ProducesProblem(StatusCodes.Status403Forbidden)
-            .ProducesValidationProblem()
-            .WithOpenApi()
-            .IncludeInOpenApi();
+            .WithOpenApi();
     }
 
     private static async Task<IResult> CreateBudget(
@@ -96,30 +73,5 @@ public class BudgetModule : ICarterModule
         var result = await mediator.SendRequest(new GetBudgetQuery(id), cancellationToken);
 
         return result.Match(x => Results.Ok(x));
-    }
-
-    private static async Task<IResult> CreateBudgetCategory(
-        IMediator mediator,
-        [FromRoute] Guid id,
-        [FromBody] CreateBudgetCategoryRequest request,
-        CancellationToken cancellationToken
-    )
-    {
-        var result = await mediator.SendRequest(new CreateBudgetCategoryCommand(id, request.Name), cancellationToken);
-
-        return result.Match(Results.Created);
-    }
-
-
-    private static async Task<IResult> ArchiveBudgetCategory(
-        IMediator mediator,
-        [FromRoute] Guid id,
-        [FromRoute] string name,
-        CancellationToken cancellationToken
-    )
-    {
-        var result = await mediator.SendRequest(new ArchiveBudgetCategoryCommand(id, name), cancellationToken);
-
-        return result.Match(Results.NoContent);
     }
 }
