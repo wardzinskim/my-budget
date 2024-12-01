35be2b6d7ce898d78cbc73948912e57cf0d8384c
Marcin Wardzinski <wardzinskim@outlook.com>
2024-12-01 11:57:47 +0100

    add health chectk to identity

--- backend/Identity/MyBudget.Identity/Installers/HealthChecksInstaller.cs
+++ backend/Identity/MyBudget.Identity/Installers/HealthChecksInstaller.cs
@@ -1,13 +1,13 @@
 ﻿using MyBudget.Infrastructure.Abstractions.Installer;
 
-namespace MyBudget.Api.Installers;
+namespace MyBudget.Identity.Installers;
 
 public sealed class HealthChecksInstaller : IInstaller
 {
     public void Install(IServiceCollection services, IConfiguration configuration, IHostEnvironment hostingEnvironment)
     {
         var healthCheckBuilder = services.AddHealthChecks();
-        
+
         healthCheckBuilder.AddNpgSql(configuration.GetConnectionString("Default")!);
     }
 
